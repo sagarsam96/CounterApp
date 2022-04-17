867d2edc88f18ebb293d0a9a1ffb64544b4df797
sagarsam96 <92667993+sagarsam96@users.noreply.github.com>
2022-04-17 14:04:10 +0530

    Updates
    updated the UI and added translateY in buttton, changing background of output with click of the button.

--- index.js
+++ index.js
@@ -7,18 +7,28 @@ let value = ~~(output.value);
 increment.addEventListener("click", (e)=>{
     e.preventDefault();
     value++;
-    output.innerHTML= value;
+    output.textContent= value;
+    outputColorChange("green");
 })
 
 
 decrement.addEventListener("click", (e)=>{
     e.preventDefault();
     value<1? value : value--;
-    output.innerHTML= value;
+    output.textContent= value;
+    outputColorChange("red");
 })
 
 reset.addEventListener("click", (e)=>{
     e.preventDefault();
     value=0;
-    output.innerHTML= value;
+    output.textContent= value;
+    outputColorChange("aqua");
 })
+
+const outputColorChange=(color)=>{
+    output.style.background=color;
+    setTimeout( ()=>{
+    output.style.background="transparent";
+    }, 200)
+}
